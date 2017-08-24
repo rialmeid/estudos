1f60dd734e4e0c10e21df3250f89c97027fa9226
Ricardo L Almeida <rlalmeida@rd.com.br>
2017-08-24 00:43:48 -0300

    termino aula 2.8

--- cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -34,6 +34,11 @@ public class TituloController {
         return mv;
     }
 
+    @RequestMapping
+    public String pesquisar() {
+        return "PesquisaTitulos";
+    }
+
     @ModelAttribute("todosStatusTitulo")
     public List<StatusTitulo> todosStatusTitulo() {
         return Arrays.asList(StatusTitulo.values());
