ca071e4b76b7b707199ddcd76c4b481f49e0e59e
Ricardo L Almeida <rlalmeida@rd.com.br>
2017-08-23 23:25:26 -0300

    first commit

---
+++ cobranca/src/main/java/com/algaworks/cobranca/model/StatusTitulo.java
@@ -0,0 +1,23 @@
+package com.algaworks.cobranca.model;
+
+public enum StatusTitulo {
+
+    PENDENTE("Pendente"),
+    RECEBIDO("Recebido"),
+    CANCELADO("Cancelado");
+
+    private String descricao;
+
+    StatusTitulo(String descricao) {
+        this.descricao = descricao;
+
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+
+
+
+}
---
+++ cobranca/src/main/java/com/algaworks/cobranca/model/Titulo.java
@@ -0,0 +1,83 @@
+package com.algaworks.cobranca.model;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+public class Titulo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    private String descricao;
+
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @Temporal(TemporalType.DATE)
+    private Date dataVencimento;
+
+    @NumberFormat(pattern = "#,##0.00")
+    private BigDecimal valor;
+
+    @Enumerated(EnumType.STRING)
+    private StatusTitulo status;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(Date dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public StatusTitulo getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusTitulo status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Titulo titulo = (Titulo) o;
+
+        return codigo != null ? codigo.equals(titulo.codigo) : titulo.codigo == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return codigo != null ? codigo.hashCode() : 0;
+    }
+}
