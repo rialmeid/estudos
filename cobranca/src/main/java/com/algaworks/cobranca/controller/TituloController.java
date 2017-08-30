90cc470583a9d1e06ee9108673919c5dd0b27b4a
Ricardo L Almeida <rlalmeida@rd.com.br>
2017-08-30 00:15:22 -0300

    conclusao ate aula 2.14

--- cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -5,6 +5,8 @@ import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,14 +25,21 @@ public class TituloController {
     @RequestMapping("/novo")
     public ModelAndView novo() {
         ModelAndView mv = new ModelAndView("CadastroTitulo");
+        mv.addObject(new Titulo());
         return mv;
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ModelAndView salvar(Titulo titulo) {
-        titulos.save(titulo);
+    public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
         ModelAndView mv = new ModelAndView("CadastroTitulo");
+
+        if (errors.hasErrors()) {
+            return mv;
+        }
+
+        titulos.save(titulo);
         mv.addObject("mensagem", "Salvo com sucesso");
+        mv.addObject(new Titulo());
         return mv;
     }
 
