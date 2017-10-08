19181ac83595b07859bdcb87e07d98beb2fd9a80
Ricardo L Almeida <rlalmeida@rd.com.br>
2017-10-07 23:58:06 -0300

    4.3 - atualizando com PUT

--- algamoney-api/src/main/java/com/algaworks/algamoney/api/resource/PessoaResource.java
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/resource/PessoaResource.java
@@ -3,18 +3,15 @@ package com.algaworks.algamoney.api.resource;
 import com.algaworks.algamoney.api.event.RecursoCriadoEvent;
 import com.algaworks.algamoney.api.model.Pessoa;
 import com.algaworks.algamoney.api.repository.PessoaRepository;
-import com.sun.org.apache.regexp.internal.RE;
+import com.algaworks.algamoney.api.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,19 +19,22 @@ import java.util.List;
 public class PessoaResource {
 
     @Autowired
-    private PessoaRepository pessoaRepository;
+    private PessoaRepository repository;
 
     @Autowired
     private ApplicationEventPublisher publisher;
 
+    @Autowired
+    private PessoaService pessoaService;
+
     @GetMapping
     public List<Pessoa> listar() {
-        return pessoaRepository.findAll();
+        return repository.findAll();
     }
 
     @PostMapping
     public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
-        Pessoa save = pessoaRepository.save(pessoa);
+        Pessoa save = repository.save(pessoa);
         Long codigo = save.getCodigo();
         publisher.publishEvent(new RecursoCriadoEvent(this, response, codigo));
         return ResponseEntity.status(HttpStatus.CREATED).body(save);
@@ -43,13 +43,24 @@ public class PessoaResource {
     @GetMapping("/{codigo}")
     public ResponseEntity<Pessoa> buscarPorCodigo(@PathVariable Long codigo) {
         ResponseEntity<Pessoa> result;
-        Pessoa pessoa = pessoaRepository.findOne(codigo);
+        Pessoa pessoa = repository.findOne(codigo);
         if (pessoa == null) {
             result = ResponseEntity.noContent().build();
-        }
-        else {
+        } else {
             result = ResponseEntity.ok().body(pessoa);
         }
         return result;
+    }
+
+    @DeleteMapping("/{codigo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long codigo) {
+        repository.delete(codigo);
+    }
+
+    @PutMapping("/{codigo}")
+    public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+        Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+        return ResponseEntity.ok(pessoaSalva);
     }
 }
---
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/service/PessoaService.java
@@ -0,0 +1,26 @@
+package com.algaworks.algamoney.api.service;
+
+import com.algaworks.algamoney.api.model.Pessoa;
+import com.algaworks.algamoney.api.repository.PessoaRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PessoaService {
+
+    @Autowired
+    private PessoaRepository repository;
+
+    public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+        Pessoa pessoaSalva = repository.findOne(codigo);
+
+        if (pessoaSalva == null) {
+            throw new EmptyResultDataAccessException(1);
+        }
+
+        BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+        return repository.save(pessoaSalva);
+    }
+}
