095a0241a133cf8dc2d4a18d7ff8f0cbc3dfc2d7
Ricardo L Almeida <rlalmeida@rd.com.br>
2017-10-14 03:12:01 -0300

    10.9 - tornando a tabela de dados responsiva

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 
 import {AppComponent} from "./app.component";
-import {ButtonModule, DataTableModule, TabViewModule} from "primeng/primeng";
+import {ButtonModule, DataTableModule, TabViewModule, TooltipModule} from "primeng/primeng";
 import {InputTextModule} from "primeng/components/inputtext/inputtext";
 
 @NgModule({
@@ -14,7 +14,8 @@ import {InputTextModule} from "primeng/components/inputtext/inputtext";
     TabViewModule,
     ButtonModule,
     InputTextModule,
-    DataTableModule
+    DataTableModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
