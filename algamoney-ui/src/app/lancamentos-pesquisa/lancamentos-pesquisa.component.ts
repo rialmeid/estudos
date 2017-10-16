afb4a07ce2d2890ac6b2a1820d58d4715bad268f
Ricardo L Almeida <rlalmeida@rd.com.br>
2017-10-16 00:50:26 -0200

    11.8 - desafio - formatar data e valor usando pipe

--- algamoney-ui/src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ algamoney-ui/src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -11,49 +11,49 @@ export class LancamentosPesquisaComponent {
     {
       tipo: 'DESPESA',
       descricao: 'Compra de pão',
-      dataVencimento: '01/01/2017',
-      dataPagamento: '',
+      dataVencimento: new Date(2017, 10, 15),
+      dataPagamento: null,
       valor: 3.50,
       pessoa: 'Padaria Natural'
     },
     {
       tipo: 'RECEITA',
       descricao: 'Fretado',
-      dataVencimento: '02/01/2017',
-      dataPagamento: '',
+      dataVencimento: new Date(2017, 10, 15),
+      dataPagamento: null,
       valor: 354.50,
       pessoa: 'Natal'
     },
     {
       tipo: 'DESPESA',
       descricao: 'Convenio',
-      dataVencimento: '03/01/2017',
-      dataPagamento: '',
+      dataVencimento: new Date(2017, 10, 15),
+      dataPagamento: null,
       valor: 366.50,
       pessoa: 'Amil'
     },
     {
       tipo: 'DESPESA',
       descricao: 'Condominio',
-      dataVencimento: '04/01/2017',
-      dataPagamento: '',
+      dataVencimento: new Date(2017, 10, 15),
+      dataPagamento: null,
       valor: 343.50,
       pessoa: 'Helbor'
     },
     {
       tipo: 'RECEITA',
       descricao: 'Carro',
-      dataVencimento: '05/01/2017',
-      dataPagamento: '',
+      dataVencimento: new Date(2017, 10, 15),
+      dataPagamento: null,
       valor: 323.50,
       pessoa: 'Bradesco'
     },
     {
       tipo: 'RECEITA',
       descricao: 'Escola',
-      dataVencimento: '06/01/2017',
-      dataPagamento: '',
-      valor: 345.50,
+      dataVencimento: new Date(2017, 10, 15),
+      dataPagamento: null,
+      valor: 1345.50,
       pessoa: 'Gutemberg'
     }
   ];
