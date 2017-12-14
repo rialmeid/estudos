efed7523ba2946dffb1ecef3f82cac93578337bc
ricardo.121072 <rlalmeida@raiadrogasil.com.br>
2017-12-14 14:00:01 -0200

    usando o profile para alternar entre basic e oauth

--- algamoney-api/src/main/java/com/algaworks/algamoney/api/config/BasicSecurityConfig.java
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/config/BasicSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.algamoney.api.config;
+package com.algaworks.algamoney.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
