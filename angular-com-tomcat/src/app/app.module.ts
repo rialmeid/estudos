6cba779dc7c49a77fe77ce10faa5a59e1a064baf
ricardo.121072 <rlalmeida@raiadrogasil.com.br>
2017-12-27 16:22:24 -0200

    labs para primeng

--- angular-com-tomcat/src/app/app.module.ts
+++ angular-com-tomcat/src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
+import { ArvoreComponent } from './arvore/arvore.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import {TreeModule, TreeTable, TreeTableModule} from "primeng/primeng";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArvoreComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    TreeModule,
+    TreeTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
